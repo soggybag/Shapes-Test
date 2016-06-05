9b41f766c95e332260c4a644db961e92d8c9c9ee
Mitchell Hudson <mitchellhudson@mitchells-mbp.gateway.sonic.net>
2016-06-05 10:55:09 -0700

    Shapes-Test
    This tests tests making UIBezier shapes to create physics bodies.

--- Shapes-Test/GameScene.swift
+++ Shapes-Test/GameScene.swift
@@ -8,35 +8,44 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: view.frame)
+        backgroundColor = SKColor.blackColor()
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        print(contact)
+    }
+    
+    
+    
+    func makeStar() -> SKSpriteNode {
+        let starImage = StarKit.imageOfStar
+        let texture = SKTexture(image: starImage)
+        let star = SKSpriteNode(texture: texture)
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        star.physicsBody = SKPhysicsBody(polygonFromPath: StarKit.getStarPath().CGPath)
+        
+        
+        return star
+    }
+    
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+        
+        guard let touch = touches.first else {
+            return
         }
+        
+        let star = makeStar()
+        addChild(star)
+        star.position = touch.locationInNode(self)
     }
    
     override func update(currentTime: CFTimeInterval) {
